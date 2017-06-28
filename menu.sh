2a043c73116dd1bfa65f438517679c10c08f4aaa
Muhamad Aiman bin Amir <blazevpn21@gmail.com>
2017-06-28 22:32:41 +0800

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Menu
+
+echo -e "================================MENU====================================="
+echo -e "* menu       : senarai perintah"
+echo -e "* usernew    : membuat akaun SSH"
+echo -e "* trial      : membuat akaun trial"
+echo -e "* hapus      : menghapus akaun SSH"
+echo -e "* login      : semak login user"
+echo -e "* member     : senarai user"
+echo -e "* resvis     : restart service dropbear, webmin, squid3, openvpn dan ssh"
+echo -e "* reboot     : reboot VPS"
+echo -e "* speedtest  : speedtest VPS"
+echo -e "* info       : informasi sistem"
+echo -e "* about      : informasi tentang script auto install"
+echo -e "========================================================================="
+echo -e ""
