eaee66ab28d5326569d6cf90dd1236c3d2fcf733
Muhamad Aiman bin Amir <blazevpn21@gmail.com>
2017-06-28 22:46:52 +0800

    Create about.sh

---
+++ about.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#About this script
+
+clear
+echo -e "===================================================="
+echo -e "Script Auto Install SSH & OpenVPN v1.0" 
+echo -e "===================================================="
+echo -e ""
+echo -e "For Debian 7 32 bit & 64 bit"
+echo -e "Only for VPS with KVM and VMWare virtualization"
+echo -e ""
+echo -e "Original script by :"
+echo -e "* Fornesia"
+echo -e "* Rzengineer"
+echo -e "* Fawzya"
+echo -e ""
+echo -e "Modified by Aiman Amir"
+echo -e ""
+echo -e "Join Telegram channel : https://t.me/BlazeVPN"
+echo -e "Telegram       : https://t.me/NamiaKai"
+echo -e ""
